08bf8196b77f85c6b68b052221cdc80a2abb2f42
bulcsuAdamy <bulcsu.adamy.j96513@mcast.edu.mt>
2025-12-17 18:02:38 +0100

    Finish importing assets, and start player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Move(); 
+    }
+
+    private void Move()
+    {
+       var deltaX = Input.GetAxis("Horizontal");
+       var newXPos = transform.position.x + deltaX;
+
+       transform.position = new Vector2(newXPos, transform.position.y); 
+    }
+}
