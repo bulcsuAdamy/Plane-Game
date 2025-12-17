fad66118b6b03ede63be914b8e39ff383ed9448d
bulcsuAdamy <bulcsu.adamy.j96513@mcast.edu.mt>
2025-12-17 19:15:14 +0100

    Background scroll update

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.02f;
+    Material myMaterial;
+    Vector2 offSet;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      myMaterial = GetComponent<Renderer>().material;
+
+      offSet = new Vector2(0f, backgroundScrollSpeed);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
