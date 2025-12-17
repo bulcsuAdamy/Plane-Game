778444d95ae5c71eb1e740d3b83116d6c138b909
bulcsuAdamy <bulcsu.adamy.j96513@mcast.edu.mt>
2025-12-17 18:08:25 +0100

    update player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+   [SerializeField] float movementSpeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +18,7 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-       var deltaX = Input.GetAxis("Horizontal");
+       var deltaX = Input.GetAxis("Horizontal") *Time.deltaTime * movementSpeed;
        var newXPos = transform.position.x + deltaX;
 
        transform.position = new Vector2(newXPos, transform.position.y); 
