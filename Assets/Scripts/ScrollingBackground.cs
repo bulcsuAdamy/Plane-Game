f5ca5d42e26433b9421d8ae12c21b0b4cdca17af
bulcsuAdamy <bulcsu.adamy.j96513@mcast.edu.mt>
2025-12-18 12:35:38 +0100

    finish scrolling background

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -17,5 +17,6 @@ public class ScrollingBackground : MonoBehaviour
     void Update()
     {
         myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+        Debug.Log(myMaterial.mainTextureOffset);
     }
 }
