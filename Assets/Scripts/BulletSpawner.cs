3cea63dbaca25460b0e47f07d7fd4b682408a339
bulcsuAdamy <bulcsu.adamy.j96513@mcast.edu.mt>
2025-12-19 13:36:48 +0100

    fixing separate obstacle spawning

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -1,24 +1,33 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 
-public class BulletSpawner : MonoBehaviour
+public class bulletSpawner : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 3f;
-    [SerializeField] float padding = 1f;
+
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] float bulletSpeed = 5f;
+    [SerializeField] int numberOfBullets = 10;
     [SerializeField] float minSpawnTime = 0.5f;
     [SerializeField] float maxSpawnTime = 2f;
-    [SerializeField] int numberOfBullets = 10;
-    [SerializeField] float startDelay = 3f;
+    [SerializeField] float startDelay = 2f;
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float padding = 1f;
+
     float xMin;
     float xMax;
     int direction = 1;
 
-    [SerializeField] GameObject pointGiverPrefab;
-    [SerializeField] float pointGiverChance = 0.3f;
+    void Start()
+    {
+        SetUpBoundaries();
+        StartCoroutine(SpawnBullets());
+    }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Update()
+    {
+        MoveSpawner();
+    }
+
     void MoveSpawner()
     {
         transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
@@ -29,57 +38,39 @@ public class BulletSpawner : MonoBehaviour
         }
     }
 
-    void SetUpBoundaries()
-    {
-        Camera cam = Camera.main;
-        xMin = cam.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
-        xMax = cam.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
-    }
-    
-    void SpawnObstacle()
-    {
-        GameObject prefabToSpawn;
-
-        if (Random.value < pointGiverChance)
-        {
-            prefabToSpawn = pointGiverPrefab;
-        }
-        else
-        {
-            prefabToSpawn = bulletPrefab;
-        }
-
-        GameObject obj = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
-
-        Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
-        if (rb != null)
-        {
-            rb.linearVelocity = Vector2.down* bulletSpeed;
-        }
-    }
-
     IEnumerator SpawnBullets()
     {
         yield return new WaitForSeconds(startDelay);
+
         for (int i = 0; i < numberOfBullets; i++)
         {
-            SpawnObstacle();
+            SpawnBullet();
 
             float waitTime = Random.Range(minSpawnTime, maxSpawnTime);
             yield return new WaitForSeconds(waitTime);
         }
     }
 
-    void Start()
+    void SpawnBullet()
     {
-        SetUpBoundaries();
-        StartCoroutine(SpawnBullets());
+        GameObject bullet = Instantiate(
+            bulletPrefab,
+            transform.position,
+            Quaternion.identity
+        );
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.down * bulletSpeed;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetUpBoundaries()
     {
-        MoveSpawner();
-        SpawnBullets();
+        Camera cam = Camera.main;
+        xMin = cam.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = cam.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
     }
 }
+
---
+++ Assets/Scripts/pointGiverSpawner.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Drawing;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] float minSpawnTime = 0.5f;
+    [SerializeField] float maxSpawnTime = 2f;
+    [SerializeField] int totalPointGivers = 10;
+    [SerializeField] float fallSpeed = 5f;
+    [SerializeField] float startDelay = 3f;
+    float xMin;
+    float xMax;
+    int direction = 1;
+
+    [SerializeField] GameObject pointGiverPrefab;
+    [SerializeField] float pointGiverChance = 0.3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void MoveSpawner()
+    {
+        transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x <= xMin || transform.position.x >= xMax)
+        {
+            direction *= -1;
+        }
+    }
+
+    void SetUpBoundaries()
+    {
+        Camera cam = Camera.main;
+        xMin = cam.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+        xMax = cam.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+    }
+    
+    void SpawnPointGiver()
+    {
+        GameObject pointGiver = Instantiate(pointGiverPrefab, transform.position, Quaternion.identity);
+
+        Rigidbody2D rb = pointGiver.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.down* fallSpeed;
+        }
+    }
+
+    IEnumerator SpawnPointGivers()
+    {
+        yield return new WaitForSeconds(startDelay);
+        
+        for (int i =0; i < totalPointGivers; i++)
+        {
+            SpawnPointGiver();
+
+            float waitTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        Level1Complete();
+    }
+
+    void Level1Complete()
+    {
+        Debug.Log("Level 1 Complete - Ready for level 2");
+    }
+    void Start()
+    {
+        SetUpBoundaries();
+        StartCoroutine(SpawnPointGivers());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveSpawner();
+        SpawnPointGivers();
+    }
+}
