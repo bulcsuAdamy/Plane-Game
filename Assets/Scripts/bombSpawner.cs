e777478ab9be41750bf27d071bca9bdee4ba5a97
bulcsuAdamy <bulcsu.adamy.j96513@mcast.edu.mt>
2025-12-19 18:10:52 +0100

    fixing more bomb spawns

--- Assets/Scripts/bombSpawner.cs
+++ Assets/Scripts/bombSpawner.cs
@@ -1,76 +1,46 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class bombSpawner : MonoBehaviour
+public class ObstacleSpawner : MonoBehaviour
 {
-
-    [SerializeField] GameObject bombPrefab;
-    [SerializeField] float bombSpeed = 5f;
-    [SerializeField] int numberOfBombs = 10;
-    [SerializeField] float minSpawnTime = 0.5f;
-    [SerializeField] float maxSpawnTime = 2f;
-    [SerializeField] float startDelay = 2f;
-    [SerializeField] float moveSpeed = 3f;
-    [SerializeField] float padding = 1f;
-
-    float xMin;
-    float xMax;
-    int direction = 1;
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] bool looping = true;
 
     void Start()
     {
-        SetUpBoundaries();
-        StartCoroutine(SpawnBullets());
+        Debug.Log("ObstacleSpawner Started");
+        StartCoroutine(SpawnWaves());
     }
 
-    void Update()
+    IEnumerator SpawnWaves()
     {
-        MoveSpawner();
-    }
-
-    void MoveSpawner()
-    {
-        transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
-
-        if (transform.position.x <= xMin || transform.position.x >= xMax)
+        do
         {
-            direction *= -1;
+            foreach (WaveConfig wave in waveConfigs)
+            {
+                yield return StartCoroutine(SpawnWave(wave));
+            }
         }
+        while (looping);
     }
 
-    IEnumerator SpawnBullets()
+    IEnumerator SpawnWave(WaveConfig wave)
     {
-        yield return new WaitForSeconds(startDelay);
+        GameObject path = Instantiate(wave.GetPathPrefab());
 
-        for (int i = 0; i < numberOfBombs; i++)
+        for (int i = 0; i < wave.GetNumberOfObstacles(); i++)
         {
-            SpawnBullet();
+            Instantiate(
+                wave.GetObstaclePrefab(),
+                path.transform.GetChild(0).position,
+                Quaternion.identity,
+                path.transform
+            );
 
-            float waitTime = Random.Range(minSpawnTime, maxSpawnTime);
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(wave.GetTimeBetweenSpawns());
         }
-    }
-
-    void SpawnBullet()
-    {
-        GameObject bomb = Instantiate(
-            bombPrefab,
-            transform.position,
-            Quaternion.identity
-        );
-
-        Rigidbody2D rb = bomb.GetComponent<Rigidbody2D>();
-        if (rb != null)
-        {
-            rb.linearVelocity = Vector2.down * bombSpeed;
-        }
-    }
-
-    void SetUpBoundaries()
-    {
-        Camera cam = Camera.main;
-        xMin = cam.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = cam.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
     }
 }
+
 
